64a68c3dcef58b2dc67a0ba50d22ba43b6c2f130
Victor Osipov <victorosipov99@mail.ru>
2017-02-05 16:47:35 +0300

    add new page

---
+++ js/scripts-lk.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+$('.owl-carousel-lk').owlCarousel({
+		items: 1,
+		margin: 26,
+		loop: true,
+		autoplay: true,
+		margin: 20,
+		nav: false,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		center: true
+});
+});
+var owl = $('.owl-carousel-lk').owlCarousel({
+		items: 1,
+		margin: 26,
+		loop: true,
+		/*autoplay: true,*/
+		margin: 20,
+		nav: false,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		center: true
+});
